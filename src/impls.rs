145889cf35b6ca69efb49e99b4b2728ee6a15bd3
John Nunley <dev@notgull.net>
2024-02-01 20:02:42 -0800

    feat: Implement traits for tokio
    Signed-off-by: John Nunley <dev@notgull.net>

--- src/impls.rs
+++ src/impls.rs
@@ -74,3 +74,97 @@ mod async_executor_impl {
         }
     }
 }
+
+#[cfg(feature = "tokio")]
+mod tokio_impl {
+    use crate::{Executor, CancellableTask, DetachableTask};
+    use tokio::runtime::{Handle, Runtime};
+    use tokio::task::JoinHandle;
+
+    use core::convert::Infallible;
+    use core::future::{Ready, ready, Future};
+    use core::pin::Pin;
+    use core::task::{Poll, Context};
+
+    /// A wrapper around a [`tokio::task::JoinHandle`] with task semantics.
+    pub struct TokioTask<T>(Option<JoinHandle<T>>);
+
+    impl<T> TokioTask<T> {
+        /// Get a reference to the inner `JoinHandle`.
+        pub fn get_ref(&self) -> &JoinHandle<T> {
+            self.0.as_ref().unwrap()
+        }
+
+        /// Get a mutable reference to the inner `JoinHandle`.
+        pub fn get_mut(&mut self) -> &mut JoinHandle<T> {
+            self.0.as_mut().unwrap()
+        } 
+
+        /// Convert to the inner join handle.
+        pub fn into_inner(mut self) -> JoinHandle<T> {
+            self.0.take().unwrap()
+        }
+    }
+
+    impl<T> Future for TokioTask<T> {
+        type Output = T;
+
+        fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+            match Pin::new(self.0.as_mut().unwrap()).poll(cx) {
+                Poll::Pending => Poll::Pending,
+                Poll::Ready(Ok(x)) => {
+                    self.0 = None;
+                    Poll::Ready(x)
+                }
+                Poll::Ready(Err(err)) => {
+                    self.0 = None;
+                    panic!("task was cancelled or panicked: {}", err)
+                }
+            }
+        }
+    }
+
+    impl<'a, T: 'a> CancellableTask<'a> for TokioTask<T> {
+        type Cancel = Ready<Option<T>>;
+
+        fn cancel(mut self) -> Self::Cancel {
+            self.get_mut().abort();
+
+            // TODO: Poll self once noop_waker is stable.
+            ready(None)
+        }
+    }
+
+    impl<T> DetachableTask for TokioTask<T> {
+        fn detach(mut self) {
+            // Dropping the tokio task automatically detaches it.
+            self.0 = None;
+        }
+    }
+
+    impl<T> Drop for TokioTask<T> {
+        fn drop(&mut self) {
+            if let Some(task) = self.0.take() {
+                task.abort();
+            }
+        }
+    }
+
+    impl<F: Future + Send + 'static> Executor<F> for Handle where F::Output: Send + 'static {
+        type Error = Infallible;
+        type Task = TokioTask<F::Output>;
+
+        fn try_spawn(&self, future: F) -> Result<Self::Task, Self::Error> {
+            Ok(TokioTask(Some(self.spawn(future))))
+        }
+    }
+
+    impl<F: Future + Send + 'static> Executor<F> for Runtime where F::Output: Send + 'static {
+        type Error = Infallible;
+        type Task = TokioTask<F::Output>;
+
+        fn try_spawn(&self, future: F) -> Result<Self::Task, Self::Error> {
+            Ok(TokioTask(Some(self.spawn(future))))
+        }
+    }
+}
